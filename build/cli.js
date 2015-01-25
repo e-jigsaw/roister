ee543eaef2e7ac298a0d85f4b6275bab61ba3541
jigsaw <jigsaw@live.jp>
2015-01-26 01:21:30 +0900

    add CLI compile feature

--- build/cli.js
+++ build/cli.js
@@ -1,10 +1,40 @@
 (function() {
-  var argv;
+  var compile, fs, program;
 
-  argv = require('minimist')(process.argv.slice(2));
+  fs = require('fs');
+
+  program = require('commander');
+
+  compile = require('./compile');
 
   module.exports = function() {
-    return console.log(argv);
+    var code, source, write;
+    program.version(require('../package.json').version).option('-c, --compile <path>', 'compile file').option('-p, --print', 'print code').option('-o, --output <path>', 'output file').parse(process.argv);
+    code = null;
+    write = {
+      flag: false,
+      path: null
+    };
+    if (program.compile) {
+      source = fs.readFileSync(program.compile);
+      code = compile(source);
+      write.flag = true;
+      write.path = program.compile.replace(/\.jade$/, '.js');
+    }
+    if (program.print) {
+      console.log(code);
+      write.flag = false;
+    }
+    if (program.output) {
+      if (!write.flag) {
+        console.error('Error: source file not found');
+      }
+      write.flag = true;
+      write.path = program.output;
+    }
+    if (write.flag) {
+      return fs.writeFileSync(write.path, code);
+    }
   };
 
 }).call(this);
--- src/cli.coffee
+++ src/cli.coffee
@@ -1,3 +1,33 @@
-argv = require('minimist')(process.argv.slice(2))
+fs = require 'fs'
+program = require 'commander'
+compile = require './compile'
 
-module.exports = -> console.log argv
+module.exports = ->
+  program
+    .version require('../package.json').version
+    .option '-c, --compile <path>', 'compile file'
+    .option '-p, --print', 'print code'
+    .option '-o, --output <path>', 'output file'
+    .parse process.argv
+
+  code = null
+  write =
+    flag: false
+    path: null
+
+  if program.compile
+    source = fs.readFileSync program.compile
+    code = compile source
+    write.flag = true
+    write.path = program.compile.replace(/\.jade$/, '.js')
+
+  if program.print
+      console.log code
+      write.flag = false
+
+  if program.output
+    unless write.flag then console.error 'Error: source file not found'
+    write.flag = true
+    write.path = program.output
+
+  if write.flag then fs.writeFileSync write.path, code
